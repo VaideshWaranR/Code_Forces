2903af89c4f81719917857e99212b80b2165ee94
VaideshWaranR <vaiv64259@gmail.com>
2025-07-20 19:21:40 +0530

    som 800 rated problems

--- main.cpp
+++ main.cpp
@@ -18,15 +18,25 @@ int power(int x, unsigned int y){
         return x * temp * temp;}
 
 void solve(){
-
+vector<string> arr;
+int cnt=0;
+for(int i=0;i<10;i++){
+    string temp;cin>>temp;
+    for(int j=0;j<10;j++){
+        if(temp[j]=='X'){
+        if(i==0 || i==9 || j==0 || j==9) cnt++;
+        else if(i==1 || i==8 || j==1 || j==8) cnt+=2;
+        else if(i==2 || i==7 || j==2 || j==7) cnt+=3;
+        else if(i==3 || i==6 || j==3 || j==6) cnt+=4;
+        else if(i==4 || i==5 || j==4 || j==5) cnt+=5;
+       }
+    }
+}
+cout<<cnt;
 }
 
 int32_t  main(){
 fast_io;
-#ifndef ONLINE_JUDGE
-freopen("input.txt", "r", stdin);
-freopen("output.txt", "w", stdout);
-#endif 
        w(tc){
            solve();
            cout<<endl;
